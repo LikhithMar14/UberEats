d9018d87bc218ced56a8fb9ab6ffacbf721f05b3
2005LIKHITH <likhithcvsrl@gmail.com>
2024-12-21 17:36:31 +0530

    Login Route Created

--- server/src/controllers/user.controller.ts
+++ server/src/controllers/user.controller.ts
@@ -3,59 +3,125 @@ import bcrypt from "bcryptjs";
 import { asyncHandler } from "../utils/asyncHandler";
 import { ApiError } from "../utils/ApiError";
 import { prisma } from "../prismaClient";
-import { generateAccessToken, generateVerificationCode } from "../utils/TokenGenerate";
+import {
+  generateAccessToken,
+  generateRefreshToken,
+  generateVerificationCode,
+} from "../utils/TokenGenerate";
 import { cookieOptions, STATUS_CODES } from "../constants";
 import { sendVerificationEmail } from "../NodeMailer/email";
 
-export const signup = asyncHandler(async (req: Request, res: Response): Promise<any> => {
+export const signup = asyncHandler(
+  async (req: Request, res: Response): Promise<any> => {
     const { contact, email, password, fullname } = req.body;
 
     if (![contact, email, password, fullname].every(Boolean)) {
-        throw new ApiError(400, "All fields are required!");
+      throw new ApiError(400, "All fields are required!");
     }
 
     const userExist = await prisma.user.findFirst({
-        where: { email },
+      where: { email },
     });
     if (userExist) {
-        throw new ApiError(400, "User already exists with this email");
+      throw new ApiError(
+        STATUS_CODES.BAD_REQUEST,
+        "User already exists with this email"
+      );
     }
 
     const hashedPassword = await bcrypt.hash(password, 10);
     const verificationToken = generateVerificationCode();
 
     const user = await prisma.user.create({
-        data: {
-            fullname,
-            email,
-            password: hashedPassword,
-            contact: (contact),
-            verificationToken,
-            verificationTokenExpiry: new Date(Date.now() + 24 * 60 * 60 * 1000),
-        }
+      data: {
+        fullname,
+        email,
+        password: hashedPassword,
+        contact: contact,
+        verificationToken,
+        verificationTokenExpiry: new Date(Date.now() + 24 * 60 * 60 * 1000),
+      },
     });
-
     const accessToken = generateAccessToken(user);
-    res.status(STATUS_CODES.CREATED).cookie('accessToken', accessToken, cookieOptions);
+
+    res
+      .status(STATUS_CODES.CREATED)
+      .cookie("accessToken", accessToken, cookieOptions);
 
     await sendVerificationEmail(email, verificationToken);
 
     const userWithoutPassword = await prisma.user.findUnique({
-        where: { email },
-        select: {
-            id: true,
-            email: true,
-            fullname: true,
-            address: true,
-            contact: true,
-            city: true,
-            country: true
-        }
+      where: { email },
+      select: {
+        id: true,
+        email: true,
+        fullname: true,
+        address: true,
+        contact: true,
+        city: true,
+        country: true,
+      },
     });
 
     return res.status(STATUS_CODES.CREATED).json({
-        success: true,
-        message: "Account created successfully",
-        user: userWithoutPassword
+      success: true,
+      message: "Account created successfully",
+      user: userWithoutPassword,
     });
-});
+  }
+);
+
+export const login = asyncHandler(
+  async (req: Request, res: Response): Promise<any> => {
+
+      const { email, password } = req.body;
+      if (![email, password].every(Boolean))
+        throw new ApiError(STATUS_CODES.BAD_REQUEST);
+      const userExist = await prisma.user.findFirst({
+        where: { email: email },
+      });
+      if (!userExist) {
+        return res.status(STATUS_CODES.BAD_REQUEST).json({
+          success: false,
+          message: "Incorrect email or password",
+        });
+      }
+      const user = userExist;
+
+      const isPasswordCorrect = await bcrypt.compare(password, user.password);
+      if (!isPasswordCorrect)
+        return res.status(STATUS_CODES.BAD_REQUEST).json({
+          sucess: false,
+          message: "Incorrect email or password",
+        });
+
+      user.lastLogin = new Date();
+
+      const accessToken = generateAccessToken(user);
+      const refreshToken = generateRefreshToken(user);
+
+      const userWithoutPassword = await prisma.user.findUnique({
+        where: { email: email },
+        select: {
+          id: true,
+          email: true,
+          fullname: true,
+          address: true,
+          contact: true,
+          city: true,
+          country: true,
+        },
+      });
+
+      res
+        .cookie("accessToken", accessToken, cookieOptions)
+        .cookie("refreshToken", refreshToken, cookieOptions)
+        .json({
+          success: true,
+          message: `Welcome back ${user.fullname}`,
+          user: userWithoutPassword,
+        });
+    } 
+    
+  
+);
--- server/src/routes/user.route.ts
+++ server/src/routes/user.route.ts
@@ -1,11 +1,11 @@
 import { Router } from 'express';
-import { signup } from '../controllers/user.controller';
+import { login, signup } from '../controllers/user.controller';
 
 const router = Router();
 
 
 
 router.route('/signin').post(signup)
-
+router.route('/login').post(login)
 
 export default router
