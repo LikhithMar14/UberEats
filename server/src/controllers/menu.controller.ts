261f8ba10c8bf929a7328c067aa932ab1aedabf6
2005LIKHITH <likhithcvsrl@gmail.com>
2024-12-22 16:04:34 +0530

    created edit menu route

--- server/src/controllers/menu.controller.ts
+++ server/src/controllers/menu.controller.ts
@@ -43,3 +43,34 @@ export const addMenu = asyncHandler(async(req:Request,res:Response):Promise<any>
     });
     
 })
+
+export const editMenu = asyncHandler(async(req:Request,res:Response):Promise<any> => {
+    const {menuId} = req.params;
+    const {name,description,price} = req.body;
+    const menuDetails = await prisma.user.findFirst({
+        where:{id:Number(menuId)},
+    })
+    if(!menuDetails)throw new ApiError(STATUS_CODES.NOT_FOUND,"Menu not found")
+    const menuImageFile = req.file
+    if(!menuImageFile)throw new ApiError(STATUS_CODES.BAD_REQUEST,"Menu image is required")
+    const menuImagePath = (<TUserFiles>req.files)?.menuImage?.[0]?.path;
+    if(!menuImagePath)throw new ApiError(STATUS_CODES.BAD_REQUEST,"Menu image is required")
+    const menuImage = await uploadFileToCloudinary(menuImagePath,{
+        folder:"Images",
+        retries:1,
+    })
+    const updatedMenu = await prisma.menu.update({
+        where:{id:menuDetails.id},
+        data:{
+            name,
+            description,
+            price,
+            image:menuImage?.secure_url!,
+        }
+    })
+    return res.status(STATUS_CODES.OK).json({
+        success:true,
+        message:"Menu updated successfully",
+        updatedMenu
+    })
+})
